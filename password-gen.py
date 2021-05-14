6d2fcc1f69eac2ad35e363080086617b5f895244
r0ydster <r0ydster@mdrcs.com>
2021-05-14 16:20:22 -0500

    Added main and loop

--- password-gen.py
+++ password-gen.py
@@ -1,31 +1,44 @@
 #!/usr/bin/env python3
 
 '''
-Password generator
-Prompt for number of characters in Password take that number, then generate a password randomizing letters, numbers and special characters.
+Python Password Generator
+Ask for the number of characters wanted in a Password, take that number,
+generate a password randomizing letters, numbers and special characters.
 '''
 
 import string
 import random
+import sys
 
-# Prompt for length of password, convert to int.
+def main():
+    print('Python Password Generator\n')
 
-print('Python Password Generator\n')
-length = int(input('Enter the length of the password: '))
+    # Prompt for length of password. 
+    length = int(input('Enter the length of the password: '))
 
-lower = string.ascii_lowercase
-upper = string.ascii_uppercase
-num = string.digits
-symbols = string.punctuation
+    lower = string.ascii_lowercase
+    upper = string.ascii_uppercase
+    num = string.digits
+    symbols = string.punctuation
 
-# Combine all types 
-all = lower + upper + num + symbols
+    # Combine all types 
+    all = lower + upper + num + symbols
 
-# randomize all 
-temp = random.sample(all,length)
+    # randomize all 
+    temp = random.sample(all,length)
 
-# Create the password
-password = "".join(temp)
+    # Create the password
+    password = "".join(temp)
 
-# Print the generated password
-print(password)
+    # Print the generated password
+    print(password)
+
+    # Infinite loop
+    choice = input('Press enter to generate another password or q to quit: ')
+    if choice == 'q':
+        sys.exit(0)
+    else:
+        main()
+
+if __name__ == "__main__":
+    main()
